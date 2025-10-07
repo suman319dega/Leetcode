3d3bcd2714b9aeda6f64cba6320d84fe15962a5b
Sumanth <163392218+suman319dega@users.noreply.github.com>
2025-10-07 10:08:14 +0530

    Time: 3 ms (62.52%), Space: 127.4 MB (24.89%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,40 +8,23 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-ListNode* rev(ListNode* head) {
-    ListNode* temp = head;
-    ListNode* prev = nullptr;
-    while(temp) {
-        ListNode* back = temp -> next;
-        temp -> next = prev;
-        prev = temp;
-        temp = back;
-    }
-    return prev;
-}
-
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if(!head) return true;
-        ListNode* fast = head;
-        ListNode* slow = head;
-        while(fast && fast -> next) {
-            fast = fast -> next -> next;
-            slow = slow -> next;
-        }
-        ListNode* rhead = rev(slow);
         ListNode* temp = head;
-        ListNode* rtemp = rhead;
-        while(rtemp) {
-            if(rtemp -> val != temp -> val) {
-                rev(rhead);
-                return false;
-            }
-            rtemp = rtemp -> next;
+        stack<int> stk;
+        while(temp) {
+            stk.push(temp -> val);
             temp = temp -> next;
         }
-        rev(rhead);
+        temp = head;
+        while(temp && !stk.empty()) {
+            int x = stk.top();
+            if(x != temp -> val) return false;
+            stk.pop();
+            temp = temp -> next;
+
+        }
         return true;
-     }
+    }
 };
