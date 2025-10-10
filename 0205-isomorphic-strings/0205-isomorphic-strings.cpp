ad46881e999cd6e1d24afa929fb3bad1e366a4c4
Sumanth <163392218+suman319dega@users.noreply.github.com>
2025-10-10 20:24:46 +0530

    Time: 3 ms (43.81%), Space: 9.6 MB (8.6%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,23 +1,16 @@
+bool help(string s, string t) {
+    if(s.size() != t.size()) return false;
+    map<char,char> m;
+    for(int i=0; i<s.size(); i++) {
+        if(m.find(s[i]) != m.end() && m[s[i]] != t[i]) return false;
+        else m[s[i]] = t[i];
+    }
+    return true;
+}
+
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.size() != t.size()) return false;
-        map<char,char> m;
-        map<char,char> r;
-        for(int i=0; i<s.size(); i++) {
-            char a = s[i];
-            char b = t[i];
-            if(m.count(a)){
-                if(m[a] != b) return false;
-            }
-            else {
-                if(r.count(b)) return false;
-            }
-
-            m[a] = b;
-            r[b] = a;
-        }
-        return true;
-        
+        return help(s,t) && help(t,s);
     }
 };
