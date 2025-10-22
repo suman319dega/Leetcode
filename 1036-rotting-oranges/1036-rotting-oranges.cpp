82310ac598053b53d79de56d231bb6a36a24b005
Sumanth <163392218+suman319dega@users.noreply.github.com>
2025-10-22 07:51:29 +0530

    Time: 1 ms (39.98%), Space: 17.1 MB (45.79%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,48 +1,46 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int ans = INT_MIN;
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> visited(n,vector<int>(m,0));
         queue<pair<pair<int,int>,int>> q;
-        vector<vector<int>> v(n,vector<int>(m,0));
+        int max_dis = INT_MIN;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(grid[i][j] == 2) {
                     q.push({{i,j},0});
-                    v[i][j] = 1;
+                    visited[i][j] = 1;
                 }
             }
         }
         while(!q.empty()) {
-            int r = q.front().first.first;
-            int c = q.front().first.second;
+            int row = q.front().first.first;
+            int col = q.front().first.second;
             int d = q.front().second;
             q.pop();
-            ans = max(ans,d);
-            if(r > 0 && grid[r-1][c] == 1 && !v[r-1][c]) {
-                q.push({{r-1,c},d+1});
-                v[r-1][c] = 1;
+            max_dis = max(d,max_dis);
+            if(row > 0 && grid[row-1][col] == 1 && !visited[row-1][col]) {
+                q.push({{row-1,col},d+1});
+                visited[row-1][col] = 1;
             }
-            if(r+1 < n && grid[r+1][c] == 1 && !v[r+1][c]) {
-                q.push({{r+1,c},d+1});
-                v[r+1][c] = 1;
+            if(row+1 < n && grid[row+1][col] == 1 && !visited[row+1][col]) {
+                q.push({{row+1,col},d+1});
+                visited[row+1][col] = 1;
             }
-            if(c > 0 && grid[r][c-1] == 1 && !v[r][c-1]) {
-                q.push({{r,c-1},d+1});
-                v[r][c-1] = 1;
+            if(col > 0 && grid[row][col-1] == 1 && !visited[row][col-1]) {
+                q.push({{row,col-1},d+1});
+                visited[row][col-1] = 1;
             }
-            if(c+1 < m && grid[r][c+1] == 1 && !v[r][c+1]) {
-                q.push({{r,c+1},d+1});
-                v[r][c+1] = 1;
+            if(col+1 < m && grid[row][col+1] == 1 && !visited[row][col+1]) {
+                q.push({{row,col+1},d+1});
+                visited[row][col+1] = 1;
             }
         }
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(grid[i][j] == 1 && !v[i][j]) return -1;
+                if(grid[i][j] == 1 && !visited[i][j]) return -1;
             }
         }
-        if(ans == INT_MIN) return 0;
-        else return ans;
+        return (max_dis == INT_MIN) ? 0 : max_dis;
     }
 };
