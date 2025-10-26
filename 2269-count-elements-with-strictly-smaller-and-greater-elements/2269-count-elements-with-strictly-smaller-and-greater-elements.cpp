afae3c490ae9be901828b4cc32afb2512769bdf9
Sumanth <163392218+suman319dega@users.noreply.github.com>
2025-10-26 11:05:16 +0530

    Time: 0 ms (100%), Space: 12.4 MB (78.5%) - LeetHub

--- 2269-count-elements-with-strictly-smaller-and-greater-elements/2269-count-elements-with-strictly-smaller-and-greater-elements.cpp
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/2269-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int countElements(vector<int>& nums) {
-        int n = nums.size();
-        int mini = *min_element(nums.begin(), nums.end());
-        int maxi = *max_element(nums.begin(), nums.end());
-        int min_count = 0, max_count = 0;
-        for(int num : nums) {
-            if(num == maxi) max_count++;
-            if(num == mini) min_count++;
+        sort(nums.begin(),nums.end());
+        int n = nums.size(), count = 0;
+        int mini = nums[0] , maxi = nums[n-1];
+        for(int i=1; i<n-1; i++) {
+            if(nums[i] > mini && nums[i] < maxi) count++;
         }
-        if(mini == maxi) return 0;
-        return  n - max_count - min_count;
+        return count;
     }
 };
