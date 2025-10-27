797077314f30599d516a43b159bfff65794b31f8
Sumanth <163392218+suman319dega@users.noreply.github.com>
2025-10-27 07:30:33 +0530

    Time: 0 ms (100%), Space: 7.7 MB (41.5%) - LeetHub

---
+++ 1920-determine-color-of-a-chessboard-square/1920-determine-color-of-a-chessboard-square.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        int a = coordinates[0] - 'a';
+        int b = coordinates[1] - '0';
+        a = a + 1;
+        if((a % 2 == 0 && b % 2 == 0) || (a % 2 != 0 && b % 2 != 0)) return false;
+        else return true;
+    }
+};
