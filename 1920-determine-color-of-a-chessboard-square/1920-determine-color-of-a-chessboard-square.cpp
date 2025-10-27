e91f3377e98538cbcab3182fc1acc61596ef3f43
Sumanth <163392218+suman319dega@users.noreply.github.com>
2025-10-27 07:40:30 +0530

    Time: 0 ms (100%), Space: 7.6 MB (71.85%) - LeetHub

--- 1920-determine-color-of-a-chessboard-square/1920-determine-color-of-a-chessboard-square.cpp
+++ 1920-determine-color-of-a-chessboard-square/1920-determine-color-of-a-chessboard-square.cpp
@@ -4,7 +4,8 @@ public:
         int a = coordinates[0] - 'a';
         int b = coordinates[1] - '0';
         a = a + 1;
-        if((a % 2 == 0 && b % 2 == 0) || (a % 2 != 0 && b % 2 != 0)) return false;
+        int sum = a + b;
+        if(sum % 2 == 0) return false;
         else return true;
     }
 };
