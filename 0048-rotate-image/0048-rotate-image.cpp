ab8ffae0dcadb27e591f06df8ed7e16dce83c6da
Sumanth <163392218+suman319dega@users.noreply.github.com>
2025-11-03 07:26:18 +0530

    Time: 0 ms (100%), Space: 10.3 MB (8.9%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,7 +4,9 @@ public:
         int n = matrix.size();
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                if(j > i) swap(matrix[i][j], matrix[j][i]);
+                if(i < j) {
+                    swap(matrix[i][j],matrix[j][i]);
+                }
             }
         }
         for(auto &temp : matrix) {
