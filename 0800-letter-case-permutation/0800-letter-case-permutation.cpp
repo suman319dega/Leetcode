d0e4ccfe64c7f063f334894183e498920be3f428
Sumanth <163392218+suman319dega@users.noreply.github.com>
2025-11-06 07:11:34 +0530

    Time: 6 ms (19.6%), Space: 14.2 MB (60.92%) - LeetHub

--- 0800-letter-case-permutation/0800-letter-case-permutation.cpp
+++ 0800-letter-case-permutation/0800-letter-case-permutation.cpp
@@ -1,27 +1,21 @@
+void recursion(string s,vector<string>& result,int id) {
+    if(id >= s.size()) {
+        result.push_back(s);
+        return;
+    }
+    recursion(s,result,id+1);
+    if(isalpha(s[id])) {
+        if(islower(s[id])) s[id] = toupper(s[id]);
+        else s[id] = tolower(s[id]);
+        recursion(s,result,id+1);
+    }
+}
+
 class Solution {
 public:
     vector<string> letterCasePermutation(string s) {
-        set<string> stores;
-        stores.insert(s);
-
-        int n = s.size();
-
-        for (int i = 0; i < n; i++) {
-            if (isalpha(s[i])) {
-                set<string> temp = stores; 
-
-                for (auto str : temp) {
-                    string t = str;
-                    if (islower(t[i]))
-                        t[i] = toupper(t[i]);
-                    else
-                        t[i] = tolower(t[i]);
-                    stores.insert(t);
-                }
-            }
-        }
-
-        vector<string> ans(stores.begin(), stores.end());
-        return ans;
+        vector<string> result;
+        recursion(s,result,0);
+        return result;
     }
 };
