1b143268c65572374c6421871a974952407cbb72
Sumanth <163392218+suman319dega@users.noreply.github.com>
2025-11-07 14:05:59 +0530

    Time: 0 ms (100%), Space: 10 MB (41.67%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,20 @@
-void sub(int i,vector<vector<int>>& ans,vector<int>&v, vector<int>& nums,int n) {
-    if(i >= n) {
+void dfs(vector<int>& v, vector<vector<int>> &ans,vector<int>& nums,int i) {
+    if(i >= nums.size()) {
         ans.push_back(v);
         return;
     }
     v.push_back(nums[i]);
-    sub(i+1,ans,v,nums,n);
+    dfs(v,ans,nums,i+1);
     v.pop_back();
-    sub(i+1,ans,v,nums,n);
+    dfs(v,ans,nums,i+1);
 }
 
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int i = 0,n = nums.size();
         vector<vector<int>> ans;
         vector<int> v;
-        sub(i,ans,v,nums,n);
+        dfs(v,ans,nums,0);
         return ans;
     }
 };
