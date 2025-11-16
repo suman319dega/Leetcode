89c25e7b0eeb7d9e3351706ae62c70375a673c43
Sumanth <163392218+suman319dega@users.noreply.github.com>
2025-11-16 09:35:29 +0530

    Time: 0 ms (100%), Space: 10.2 MB (71.06%) - LeetHub

---
+++ 0857-positions-of-large-groups/0857-positions-of-large-groups.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> largeGroupPositions(string s) {
+        vector<vector<int>> ans;
+        int n = s.size();
+        int i = 0;
+        while(i < n) {
+            int j = i;
+            while(j < n && s[i] == s[j]) {
+                j++;
+            }
+            if(j - i >= 3) {
+                ans.push_back({i,j-1});
+            }
+            i = j;
+        }
+        return ans;
+    }
+};
