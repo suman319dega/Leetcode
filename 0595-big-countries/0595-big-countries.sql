c20f0ec8dc600aeb7b854f02c82389e61bc567ba
Sumanth <163392218+suman319dega@users.noreply.github.com>
2025-11-25 14:34:38 +0530

    Time: 277 ms (63.46%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,2 +1,2 @@
-select name,population,area from World
-where population  >= 25000000 or area  >= 3000000;
+select name, population, area from World
+where  area >= 3000000 or population >= 25000000;
