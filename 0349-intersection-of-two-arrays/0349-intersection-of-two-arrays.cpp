db964b3912b3eaad3380560b5a01585caf4f750f
Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-01-06 08:51:12 +0530

    Time: 0 ms (100%), Space: 14 MB (70.35%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -6,15 +6,15 @@ public:
         int i = 0, j = 0;
         vector<int> ans;
         while(i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] == nums2[j]) {
-                if (ans.empty() || ans.back() != nums1[i]) {
+            if(nums1[i] == nums2[j]) {
+                if(ans.empty() || ans.back() != nums1[i]) {
                     ans.push_back(nums1[i]);
                 }
                 i++;
                 j++;
             }
             else if(nums1[i] < nums2[j]) i++;
-            else  j++;
+            else j++;
 
         }
         return ans;
