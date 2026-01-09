4edba284865da6d64ab70615a16954068ce3a12b
Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-01-09 21:12:50 +0530

    Time: 0 ms (100%), Space: 11.7 MB (46.99%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        // vector<int> ans;
         int i = 0;
-        for(int j = 0; j<nums.size(); j++) {
+        for(int j=0; j<nums.size(); j++) {
             if(nums[j] != val) {
                 nums[i++] = nums[j];
             }
