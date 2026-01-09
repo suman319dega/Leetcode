aaa2a04670a98960addcb8ede5c18c68ac407fb8
Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-01-09 21:09:52 +0530

    Time: 0 ms (100%), Space: 13.5 MB (97.59%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -5,7 +5,7 @@ public:
         while(low <= high) {
             int mid = (low + high) / 2;
             if(nums[mid] == target) return mid;
-            if(nums[mid] > target) high = mid - 1;
+            else if(nums[mid] > target) high = mid - 1;
             else low = mid + 1;
         }
         return low;
