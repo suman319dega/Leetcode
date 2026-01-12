5edb8969f8323b19a10544127d053f0ef60bcf96
Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-01-12 08:19:31 +0530

    Time: 3 ms (13.74%), Space: 18.2 MB (54.77%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,36 @@
-void dfs(int n, int m,int start,vector<vector<int>>& image,int color, int i, int j) {
-    image[i][j] = color;
-    if(i > 0 && image[i-1][j] == start) {
-        dfs(n,m,start,image,color,i-1,j);
-    }
-    if(i+1 < n && image[i+1][j] == start) {
-        dfs(n,m,start,image,color,i+1,j);
-    }
-    if(j+1 < m && image[i][j+1] == start) {
-        dfs(n,m,start,image,color,i,j+1);
-    }
-    if(j > 0 && image[i][j-1] == start) {
-        dfs(n,m,start,image,color,i,j-1);
-    }
-}
-
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
-        int start = image[sr][sc];
-        if(start == color) return image;
-        dfs(n,m,start,image,color,sr,sc);
+        int n = image.size(), m = image[0].size();
+        int x = image[sr][sc];
+        if(x == color) return image;
+
+        queue<pair<int,int>> q;
+        q.push({sr, sc});
+        image[sr][sc] = color;
+
+        while(!q.empty()) {
+            int a = q.front().first;
+            int b = q.front().second;
+            q.pop();
+
+            if(a-1 >= 0 && image[a-1][b] == x) {
+                image[a-1][b] = color;
+                q.push({a-1, b});
+            }
+            if(a+1 < n && image[a+1][b] == x) {
+                image[a+1][b] = color;
+                q.push({a+1, b});
+            }
+            if(b-1 >= 0 && image[a][b-1] == x) {
+                image[a][b-1] = color;
+                q.push({a, b-1});
+            }
+            if(b+1 < m && image[a][b+1] == x) {
+                image[a][b+1] = color;
+                q.push({a, b+1});
+            }
+        }
         return image;
     }
 };
