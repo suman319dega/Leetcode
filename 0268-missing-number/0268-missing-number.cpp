2297ce214cf5539e968ff6727ec79bc23b021c11
Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-01-29 11:48:12 +0530

    Time: 0 ms (100%), Space: 21.7 MB (69.1%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        long long sum1 = (n * (n + 1) ) / 2;
-        long long sum2 = 0;
-        for(int num : nums) {
-            sum2 += num;
-        }
-        return abs(sum2 - sum1);
+        long long  sum1 = 0;
+        for(int num : nums) sum1 += num;
+        long long sum2 = (n * (n+1)) / 2;
+        return abs(sum1 - sum2);
+        
     }
 };
