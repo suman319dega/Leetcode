64ed3ded479ff2225363e619f20f0cc8a0625b2b
Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-01-29 11:54:37 +0530

    Time: 0 ms (100%), Space: 24 MB (19.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,9 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         int i = 0;
         for(int j=0; j<nums.size(); j++) {
-            if(nums[j] != 0) {
-                swap(nums[j],nums[i++]);
-            }
+            if(nums[j] != 0) swap(nums[j],nums[i++]);
         }
     }
 };
