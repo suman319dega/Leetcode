0507125e8d645432afe957fc7bb07d020b6ef4fe
Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-03-30 13:32:04 +0530

    Time: 3 ms (67.39%), Space: 15.1 MB (11.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,9 +4,7 @@ public:
         map<int,int> m;
         for(int i=0; i<nums.size(); i++) {
             int x = target - nums[i];
-            if(m.find(x) != m.end()) {
-                return {i,m[x]};
-            }
+            if(m.find(x) != m.end()) return {m[x],i};
             m[nums[i]] = i;
         }
         return {-1,-1};
