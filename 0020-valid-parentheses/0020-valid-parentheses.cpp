633b36f42a004e22dbbaae051756c4e38bc94ecf
Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-03-30 13:43:31 +0530

    Time: 0 ms (100%), Space: 9 MB (38.72%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,14 +3,13 @@ public:
     bool isValid(string s) {
         stack<char> stk;
         for(char ch : s) {
-            if(ch == '(' || ch == '{' || ch == '[') stk.push(ch);
+            if(ch == '[' || ch == '(' || ch == '{') stk.push(ch);
             else {
                 if(stk.empty()) return false;
                 char x = stk.top();
                 stk.pop();
                 if((x == '(' && ch == ')') || 
-                    (x == '{' && ch == '}') ||
-                    (x == '[' && ch == ']') ) continue;
+                (x == '[' && ch == ']') || (x == '{' && ch == '}')) continue;
                 else return false;
             }
         }
