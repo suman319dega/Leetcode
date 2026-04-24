92ab0052705ed0aaf10371dfdfe20eb0d8297a35
Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-04-24 13:18:11 +0530

    Time: 0 ms (100%), Space: 28.2 MB (87.86%) - LeetHub

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        vector<int> ans;
+        for(int num : nums) {
+            if(num % 2 == 0) ans.push_back(0);
+            else ans.push_back(1);
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
