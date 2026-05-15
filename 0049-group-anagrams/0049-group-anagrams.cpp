8f50e1ff7b8737089c66888b4389c9d8d448691e
Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-05-15 12:30:37 +0530

    Time: 24 ms (26.63%), Space: 26.3 MB (29.6%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,12 +3,12 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
         map<string,vector<string>> m;
-        for(string &s : strs) {
-            string key = s;
-            sort(key.begin(),key.end());
-            m[key].push_back(s);
+        for(string s : strs) {
+            string x = s;
+            sort(x.begin(),x.end());
+            m[x].push_back(s);
         }
-        for(auto &val : m) {
+        for(auto val : m) {
             ans.push_back(val.second);
         }
         return ans;
