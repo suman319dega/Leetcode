778f877e9642d3f9d5a24a272bf9c56e1f44ea24
Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-05-24 08:03:42 +0530

    Time: 0 ms (100%), Space: 8.2 MB (72.92%) - LeetHub

---
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string modifyString(string s) {
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '?') {
+                for (char ch = 'a'; ch <= 'c'; ch++) {
+                    if (i > 0 && s[i - 1] == ch) continue;
+                
+                    if (i < n - 1 && s[i + 1] == ch) continue;
+                    
+                    s[i] = ch;
+                    break;
+                }
+            }
+        }
+        
+        return s;
+    }
+};
