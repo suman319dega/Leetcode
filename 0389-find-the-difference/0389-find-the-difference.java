d2bf19d2cb90229dd2c9e5535ed92c83a2509826
Dega Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-07-05 12:07:22 +0530

    Time: 2 ms (66.44%), Space: 43.4 MB (19.83%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int arr[] = new int[26];
+        for(char ch : s.toCharArray()) arr[ch - 'a']++;
+        for(char ch : t.toCharArray()) {
+            if(arr[ch - 'a'] == 0) return ch;
+            arr[ch - 'a']--;
+        }
+        return '0';
+
+    }
+}
