f74bb4119dacf10e1a9c66380c0e2f346fde44dc
Dega Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-07-11 15:05:24 +0530

    Time: 6 ms (87.21%), Space: 47.4 MB (68.19%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -2,20 +2,20 @@ class Solution {
     public int countCharacters(String[] words, String chars) {
         int arr[] = new int[26];
         for(char ch : chars.toCharArray()) arr[ch - 'a']++;
-        int ans = 0;
+        int sum = 0;
         for(String s : words) {
-            boolean ex = true;
+            boolean x = true;
             int v[] = new int[26];
             for(char ch : s.toCharArray()) {
                 int id = ch - 'a';
                 v[id]++;
                 if(v[id] > arr[id]) {
-                    ex = false;
+                    x = false;
                     break;
                 }
             }
-            if(ex) ans += s.length();
+            if(x) sum += s.length();
         }
-        return ans;
+        return sum;
     }
 }
