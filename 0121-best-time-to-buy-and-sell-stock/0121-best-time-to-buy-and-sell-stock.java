fa31001211cc980686a2aaec50cbbe500b9f55fc
Dega Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-07-11 15:36:06 +0530

    Time: 2 ms (42.07%), Space: 94.4 MB (51.27%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max_profit = 0;
+        int price = prices[0];
+        for(int i=1; i<prices.length; i++) {
+            max_profit = Math.max(max_profit,(prices[i] - price));
+            price = Math.min(price,prices[i]);
+        }
+        return max_profit;
+    }
+}
