b4d20c35055db66be1f663922408fcbfbbc245d0
Dega Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-07-19 17:30:44 +0530

    Time: 1 ms (99.96%), Space: 77.3 MB (48.61%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max_sum = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int num : nums) {
+            sum += num;
+            max_sum = Math.max(sum,max_sum);
+            if(sum < 0) sum = 0;
+        }
+        return max_sum;
+    }
+}
