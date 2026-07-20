bfc9850cb365db611f68eb53826064c4ff72a58b
Dega Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-07-20 12:01:58 +0530

    Time: 2 ms (99.6%), Space: 44.7 MB (55.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        int v[] = new int[26];
-        for(char ch : s.toCharArray()) v[ch - 'a']++;
-        for(char ch : t.toCharArray()) v[ch - 'a']--;
-        for(int num : v) {
-            if(num != 0) return false;
+        int arr[] = new int[26];
+        for(char ch : s.toCharArray()) arr[ch - 'a']++;
+        for(char ch : t.toCharArray()) arr[ch - 'a']--;
+        for(int i=0; i<26; i++) {
+            if(arr[i] != 0) return false;
         }
         return true;
+        
     }
 }
