75c4a581fd9a3224fd9c3245220384d2c72ecfa0
Dega Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-07-21 15:45:07 +0530

    Time: 0 ms (100%), Space: 43.9 MB (34.89%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rev(int arr[]) {
+        int low = 0, high = arr.length-1;
+        while(low < high) {
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int arr[] : matrix) {
+            rev(arr);
+        }
+    }
+}
