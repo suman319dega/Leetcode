a3fda6dee4cd0ee0a5a4e115d857aa390a5c6419
Dega Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-07-22 11:15:19 +0530

    Time: 2 ms (66.26%), Space: 43.2 MB (53.88%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,12 +1,12 @@
 class Solution {
     public char findTheDifference(String s, String t) {
+        String x = "abcdefghijklmnopqrstuvwxyz";
         int arr[] = new int[26];
         for(char ch : s.toCharArray()) arr[ch - 'a']++;
-        for(char ch : t.toCharArray()) {
-            if(arr[ch - 'a'] == 0) return ch;
-            arr[ch - 'a']--;
+        for(char ch : t.toCharArray()) arr[ch - 'a']--;
+        for(int i=0; i<26; i++) {
+            if(arr[i] != 0) return x.charAt(i);
         }
         return '0';
-
     }
 }
