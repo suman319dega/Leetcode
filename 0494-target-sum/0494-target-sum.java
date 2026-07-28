4b17c404cfb7764ba02ef51b3254ae37f7a1c8d7
Dega Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-07-28 11:36:34 +0530

    Time: 2 ms (99.92%), Space: 43 MB (79.51%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for(int num : nums) sum += num;
+        if(Math.abs(target) > sum) return 0;
+        if((sum + target) % 2 != 0) return 0;
+        int k = (sum + target) / 2;
+        int dp[] = new int[k+1];
+        dp[0] = 1;
+        for(int num : nums) {
+            for(int j=k; j>=num; j--) {
+                dp[j] += dp[j-num];
+            }
+        }
+        return dp[k];
+    }
+}
