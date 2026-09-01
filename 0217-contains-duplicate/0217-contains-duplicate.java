79f129b6aec1bd59e0f433e92cfe0c80ac750224
Dega Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-09-01 10:09:10 +0530

    Time: 17 ms (88.35%), Space: 111.5 MB (5.59%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+        if(n <= 1) return false;
+        HashSet<Integer> set = new HashSet<>();
         for(int num : nums) {
-            if(map.containsKey(num)) return true;
-            map.put(num,map.getOrDefault(num,0)+1);
+            if(set.contains(num)) return true;
+            set.add(num);
         }
         return false;
     }
