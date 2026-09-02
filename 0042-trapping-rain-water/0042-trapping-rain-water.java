9acf0c5c1964acab8e26b68283f862456dcf94b7
Dega Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-09-02 16:23:50 +0530

    Time: 0 ms (100%), Space: 47.9 MB (43.8%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,22 +1,22 @@
 class Solution {
     public int trap(int[] arr) {
         int n = arr.length;
-        int leftmax = 0, rightmax = 0;
-        int left = 0, right = n-1;
         int rain = 0;
+        int left = 0, right = n-1;
+        int left_max = 0, right_max = 0;
         while(left <= right) {
-            if(leftmax <= rightmax) {
-                if(arr[left] > leftmax) {
-                    leftmax = arr[left];
+            if(left_max <= right_max) {
+                if(left_max < arr[left]) {
+                    left_max = arr[left];
                 }
-                else rain += (leftmax - arr[left]);
+                else rain += left_max - arr[left];
                 left++;
             }
             else {
-                if(arr[right] > rightmax) {
-                    rightmax = arr[right];
+                if(right_max < arr[right]) {
+                    right_max = arr[right];
                 }
-                else rain += (rightmax - arr[right]);
+                else rain += right_max - arr[right];
                 right--;
             }
         }
