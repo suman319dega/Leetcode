cfc759ec31c1a313aab5773ab2a4b71d33563f77
Dega Sumanth <163392218+suman319dega@users.noreply.github.com>
2026-09-05 10:27:10 +0530

    Time: 2 ms (89.03%), Space: 47.8 MB (80.67%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int max_reach = 0;
-        for(int i=0; i<nums.length; i++) {
-            if(i > max_reach) return false;
-            max_reach = Math.max(max_reach, (i+nums[i]));
+        int n = nums.length;
+        int maxi = 0;
+        for(int i=0; i<n; i++) {
+            if(i > maxi) return false;
+            maxi = Math.max(maxi,i+nums[i]);
         }
         return true;
     }
